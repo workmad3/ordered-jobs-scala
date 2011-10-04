8490051ea43a0fd37364a0ff7a3463affd41a621
David Workman <workmad3@gmail.com>
2011-10-04 23:13:17 +0100

    Refactored and got test 4 working

--- src/main/scala/com/workmad3/orderedJobs.scala
+++ src/main/scala/com/workmad3/orderedJobs.scala
@@ -4,7 +4,37 @@ object orderedJobs {
   def parse(jobSpec: String):List[String] = {
     if (jobSpec.compareTo("") == 0)
       List[String]()
-    else
-      List("a")
+    else {
+      var jobList = new JobList
+      jobSpec.lines.foreach(jobList.createJob(_))
+      jobList.toList
+    }
   }
+}
+
+class JobList {
+  var jobs = List[Job]();
+
+  def createJob(job: String) = {
+    val spec = job.split("=>").map(_.stripPrefix(" ").stripSuffix(" "))
+    if (spec.size > 1) {
+      addJob(new Job(spec(1)))
+      addJob(new Job(spec(0)))
+    }
+    else
+      addJob(new Job(spec(0)))
+  }
+
+  def addJob(job: Job) = {
+    if (!jobs.exists(_.name == job.name))
+      jobs = jobs :+ job
+  }
+
+  def toList():List[String] = jobs.map(_.toString)
+}
+
+class Job(jobName: String) {
+  val name = jobName
+
+  override def toString = name
 }
--- src/test/scala/com/workmad3/orderedJobsTest.scala
+++ src/test/scala/com/workmad3/orderedJobsTest.scala
@@ -9,4 +9,16 @@ class orderedJobsTest extends SpecificationWithJUnit {
   "string 'a =>' should contain the job 'a'" in {
     orderedJobs.parse("a =>") must haveTheSameElementsAs(List("a"))
   }
+
+  "string 'a =>\nb =>\nc =>' should contain the jobs 'a', 'b' and 'c'" in {
+    orderedJobs.parse("a =>\nb =>\nc =>") must haveTheSameElementsAs(List("a", "b", "c"))
+  }
+
+  "string 'a =>\nb => c\n c=>' should contain the jobs 'a', 'b' and 'c'" in {
+    orderedJobs.parse("a =>\nb => c\nc =>") must haveTheSameElementsAs(List("a", "b", "c"))
+  }
+
+  "string 'a =>\nb => c\n c=>' should contain the job 'c' before the job 'b'" in {
+    orderedJobs.parse("a =>\nb => c\nc =>") must containInOrder(List("c", "b"))
+  }
 }
